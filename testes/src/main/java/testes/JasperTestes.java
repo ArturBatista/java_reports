28ecde77a3e99b654aa24efc32fb78328d58a88c
ArturBatista <35811473+ArturBatista@users.noreply.github.com>
2021-11-12 13:29:58 -0300

    Classe para gerar relatórios do banco

---
+++ testes/src/main/java/testes/JasperTestes.java
@@ -0,0 +1,65 @@
+package testes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class JasperTestes {
+	
+	private static final String url = "jdbc:mysql://127.0.0.1/test";
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String login = "root";
+	private static final String pwd = "";
+	
+	public void gerar( String layout ) throws JRException , SQLException, ClassNotFoundException {
+		//gerando o jasper design
+		JasperDesign desenho = JRXmlLoader.load( layout );
+		
+		//compila o relatório
+		JasperReport relatorio = JasperCompileManager.compileReport( desenho );
+		
+		//estabelece conexão
+		Class.forName( driver );
+		Connection con = DriverManager.getConnection( url , login , pwd );
+		Statement stm = con.createStatement();
+		String query = "select * from aluno";
+		ResultSet rs = stm.executeQuery( query );
+		
+		//implementação da interface JRDataSource para DataSource ResultSet
+		JRResultSetDataSource jrRS = new JRResultSetDataSource( rs );
+		
+		//executa o relatório
+		Map parametros = new HashMap();
+		parametros.put("nota", new Double(10));
+		JasperPrint impressao = JasperFillManager.fillReport( relatorio , parametros, jrRS );
+		
+		//exibe o resultado
+		JasperViewer viewer = new JasperViewer( impressao , true );
+		//viewer.show();
+		viewer.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			//System.out.println("teste"); t.jrxml" );
+			new JasperTestes().gerar( "D:/teste_jp.jrxml" );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
